519f5242a37310b1b3d89329dc1dc2237dfe749d
mrrahbarnia <mrrahbarnia@gmail.com>
2023-08-08 13:05:05 +0330

    accounts,website apps and urls
    1-Started first app as accounts
    2-Created custom user class in accounts app to use email instead of username
    3-Set core urls for development and include all apps
    4-Started the second app named website
    5-Set all urls which I need for website app

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom user config
+AUTH_USER_MODEL = "accounts.User"
---
+++ core/website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    index_view, contact_view, about_view
+    )
+
+app_name = "website"
+
+urlpatterns = [
+    path('', index_view, name = "index"),
+    path('contact/', contact_view, name = "contact"),
+    path('about/', about_view, name = "about")
+]
